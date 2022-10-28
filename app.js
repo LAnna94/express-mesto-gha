5f9eb2490f2a93c9bf4b575ee164c5826cfcfa85
Anna Lahmotko <annalahmotko@yandex.ru>
2022-10-28 23:56:21 +0300

    Добавить роуты, контроллеры, модели

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-app.listen(PORT);
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(bodyParser.json());
+app.use('/users', router);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+const User = require('../models/users');
+
+// получение всех пользователей
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+// получение пользователя по Id
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params._id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+// создание нового пользователя
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
