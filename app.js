df0719565c62c9ead4758943186b93435ecd4a06
Anna Lahmotko <annalahmotko@yandex.ru>
2022-11-24 21:56:05 +0300

    добавлены логи

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const { login, createUser } = require('./controllers/users');
 const { celebrateBodyUser, celebrateLogin } = require('./validators/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
+
+app.use(requestLogger);
 
 app.post('/signin', celebrateLogin, login);
 app.post('/signup', celebrateBodyUser, createUser);
@@ -24,6 +27,9 @@ app.use('/cards', auth, cardsRouter);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use((err, req, res, next) => {
   const status = err.statusCode || constants.HTTP_STATUS_INTERNAL_SERVER_ERROR;
